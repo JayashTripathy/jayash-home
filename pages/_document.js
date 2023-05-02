acfe0d137d10ac05a8604c467be18f290ef8b405
JayashTripathy <jayashtripathy371@gmail.com>
2023-05-02 11:19:14 +0530

    added favicon and updated title

--- pages/_document.js
+++ pages/_document.js
@@ -1,14 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en" className="">
-      <Head />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width"></meta>
+      <Head/>
+      
+        <title>Jayash Space</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
