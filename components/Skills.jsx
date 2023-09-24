7785e0db61ce2720f88a73619295dd818d7be04c
JayashTripathy <jayashtripathy371@gmail.com>
2023-09-24 16:39:50 +0530

    added react query

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -18,6 +18,10 @@ const skills = [
     img: "react",
   },
   {
+    name: "React Query",
+    img: "react-query",
+  },
+  {
     name: "React Router",
     img: "react-router",
   },
@@ -61,9 +65,12 @@ export default function Skills() {
     <div className="w-[100%]   px-6">
       <h1 className="text-6xl italic font-black mb-10 ">expertise.</h1>
       <div className="grid gap-4 grid-cols-[repeat(auto-fit,_minmax(100px,_1fr))] ">
-        {skills.map(({ name, img }) => {
+        {skills.map(({ name, img }, ind) => {
           return (
-            <div className="group cursor-pointer  relative h-40 w-full dark:bg-gray-900 bg-gray-200 border-2 border-primary rounded-3xl flex justify-center items-center flex-col gap-1 dark:text-secondary">
+            <div
+              key={ind + name}
+              className="group cursor-pointer  relative h-40 w-full dark:bg-gray-900 bg-gray-200 border-2 border-primary rounded-3xl flex justify-center items-center flex-col gap-1 dark:text-secondary"
+            >
               <img src={`./icons/${img}.svg`} className="max-w-[5rem]" alt="" />
               <div className="text-xs font-bold text-gray-500 absolute top-20 opacity-0 group-hover:top-4  group-hover:opacity-100 transition-all duration-100 ease-in-out ">
                 {name}
