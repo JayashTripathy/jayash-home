5a3d33a32a5d0f24da19c1ac5ae8ec738d992ed1
JayashTripathy <jayashtripathy371@gmail.com>
2023-11-14 12:14:12 +0530

    Added prettier config and fixed husky

--- pages/connect.jsx
+++ pages/connect.jsx
@@ -3,43 +3,43 @@ import { AiFillMail } from "react-icons/ai";
 import { RiTwitterXLine } from "react-icons/ri";
 
 function connect() {
-  return (
-    <div className="mx-auto max-w-[45%] min-h-[25rem] my-16 md:max-w-[85%]">
-      <h1>👋 say hi </h1>
-      <p className="pt-8">
-        I enjoy connecting with other developers, drop me a message and let's
-        talk!
-        <br />
-        <br />
-        Social media is great for informal chats.
-        <br /> Email works best for in depth discussions.
-      </p>
+    return (
+        <div className="mx-auto max-w-[45%] min-h-[25rem] my-16 md:max-w-[85%]">
+            <h1>👋 say hi </h1>
+            <p className="pt-8">
+                I enjoy connecting with other developers, drop me a message and
+                let's talk!
+                <br />
+                <br />
+                Social media is great for informal chats.
+                <br /> Email works best for in depth discussions.
+            </p>
 
-      <div className="pt-8 text-lg md:text-base">
-        <div className="connect-link flex items-center gap-3   m">
-          <AiFillMail size={30} />
-          <a
-            href="mailto:jayashtripathy371@gmail.com"
-            className="font-bold  p-redirect"
-            target="_blank"
-          >
-            jayashtripathy371@gmail.com
-          </a>
+            <div className="pt-8 text-lg md:text-base">
+                <div className="connect-link flex items-center gap-3   m">
+                    <AiFillMail size={30} />
+                    <a
+                        href="mailto:jayashtripathy371@gmail.com"
+                        className="font-bold  p-redirect"
+                        target="_blank"
+                    >
+                        jayashtripathy371@gmail.com
+                    </a>
+                </div>
+                <br />
+                <div className="connect-link flex items-center gap-3 ">
+                    <RiTwitterXLine size={30} />
+                    <a
+                        href="https://twitter.com/jayash_tripathy"
+                        className="font-bold  p-redirect"
+                        target="_blank"
+                    >
+                        jayash_tripathy
+                    </a>
+                </div>
+            </div>
         </div>
-        <br />
-        <div className="connect-link flex items-center gap-3 ">
-          <RiTwitterXLine size={30} />
-          <a
-            href="https://twitter.com/jayash_tripathy"
-            className="font-bold  p-redirect"
-            target="_blank"
-          >
-            jayash_tripathy
-          </a>
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default connect;
